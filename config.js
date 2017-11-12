e8d0f9f40c7f48126cd49fc8aa1c17b970fe30f2
Kai Kramer <kai@gaulstall.de>
2017-11-13 00:11:14 +0100

    fix format

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = require('yargs')
   .usage(pkg.name + ' ' + pkg.version + '\n' + pkg.description + '\n\nUsage: $0 [options]')
   .describe('v', 'possible values: "error", "warn", "info", "debug"')
   .describe('n', 'instance name. used as mqtt client id and as prefix for connected topic')
-  .describe('u', 'mqtt broker url. See https://github.com/mqttjs/MQTT.js#connect-using-a-url')  
+  .describe('u', 'mqtt broker url. See https://github.com/mqttjs/MQTT.js#connect-using-a-url')
   .describe('e', 'Vorkwer user email')
   .describe('p', 'Vorwerk password')
   .alias({
